b25911fc5070f476500308e30765e2f46d19d838
Dante Martinez <dedisainer@gmail.com>
2018-06-22 18:44:52 -0500

    Inpu output y viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 
 //modulos 
@@ -9,22 +10,33 @@ import { ProgressComponent } from "./progress/progress.component";
 import { Graficas1Component } from "./graficas1/graficas1.component";
 import { PAGES_ROUTES } from "./pages.routes";
 
+//graficas
+import { ChartsModule } from 'ng2-charts';
+
+//temporal 
+import { IncrementadorComponent } from "../components/incrementador/incrementador.component";
+import { GraficodonaComponent } from "../components/graficodona/graficodona.component";
 
 @NgModule({
 declarations: [
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent,
+    GraficodonaComponent
      ],
 exports:[
      DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    GraficodonaComponent
 ],
 imports:[
      SharedModule,
-     PAGES_ROUTES
+     PAGES_ROUTES,
+     FormsModule,
+     ChartsModule
 ]
 })
 export class PagesModule {}
